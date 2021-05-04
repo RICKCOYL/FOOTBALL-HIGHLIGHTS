4af44fd883ea3573d9d3b32c5b6111d6164a6f40
RICKCOYL <rickoburu@gmail.com>
2021-05-04 11:02:38 +0300

    Finish details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-danger */
 /* eslint-disable react/jsx-key */
 /* eslint-disable prefer-const */
 /* eslint-disable no-console */
@@ -6,7 +7,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-
+import showdown from 'showdown';
 import { useParams } from 'react-router';
 import { fetchStock } from '../actions/index';
 
@@ -20,16 +21,19 @@ const Details = ({
   }, []);
 
   let arrDetails = [];
+  let converter = new showdown.Converter();
 
   const details = userData.map((e) => ({
     title: e.title,
-    embed: e.embed,
+    embed: converter.makeHtml(e.embed),
     url: e.url,
     thumbnail: e.thumbnail,
     date: e.date,
     side1: e.side1.name,
     side2: e.side2.name,
     videos: e.videos.map((e) => e.embed),
+    competition: e.competition.name,
+    competition_url: e.competition.url,
   }));
 
   const filterDetails = details.filter((e) => (title === e.title ? arrDetails.push({
@@ -41,6 +45,8 @@ const Details = ({
     side1: e.side1,
     side2: e.side2,
     videos: e.videos,
+    competition: e.competition,
+    competition_url: e.competition_url,
   }) : 'nothing'));
 
   console.log(arrDetails);
@@ -48,18 +54,31 @@ const Details = ({
   return (
     <>
       {arrDetails.map((e) => (
+        <div>
+          <div className="d-flex justify-content-between">
+            <div className="">
+              <h1>
+                {title}
+              </h1>
+            </div>
+            <div>
+              <h4 className="comp-name">
+                {e.competition}
+                {' '}
+                :
+                <a href={e.competition_url} className="btn btn-info">live scores</a>
+              </h4>
+            </div>
+            <div>
+              <a href={e.url} className="btn btn-primary" target="_blank" rel="noreferrer">HIGHLIGHTS</a>
+            </div>
 
-        <div className="d-flex justify-content-between">
-          <div className="">
-            <h1>
-              {title}
-            </h1>
           </div>
+
           <div>
-            <a href={e.url} className="btn btn-primary" target="_blank" rel="noreferrer">HIGHLIGHTS</a>
+            <div id="embed" dangerouslySetInnerHTML={{ __html: e.embed }} />
           </div>
         </div>
-
       ))}
 
     </>
