9a55a38de9561fd07d0ce17719f625917eb79c37
RICKCOYL <rickoburu@gmail.com>
2021-05-08 17:47:49 +0300

    Add test for filter.js

---
+++ src/reducers/__test__/filter.test.js
@@ -0,0 +1,17 @@
+import { changefilter } from '../../actions/index';
+import catfilter from '../filter';
+
+describe('filterReducer', () => {
+    const payload = 'Shot';
+    it('returns a category to filter when action is "FILTER"', () => {
+        expect(catfilter('', changefilter(payload))).toEqual('Shot');
+    });
+
+    it('returns the current state if "FILTER" is not the action received', () => {
+        expect(catfilter('State', { type: 'OTHER' })).toEqual('State');
+    });
+
+    it('returns an empty string of the state passed is undefined, and action is not "FILTER', () => {
+        expect(catfilter(undefined, { type: 'OTHER' })).toBe('ARMENIA: First division');
+    });
+});
